e9ba8ad8fb48bd515ec4d480636e8a6834ab129a
0x-2FA <2fa_gh@protonmail.com>
2022-01-12 16:45:42 +0200

    Created a socket file descriptor

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void)
 {
+  struct sockaddr_un address;
+
+  int socketfd = CreateClientSocket(AF_UNIX, SOCK_STREAM, 0);
+
+  address.sun_family = AF_UNIX;
+  strcpy(address.sun_path, "server_socket");
   
   return 0;
 }
