e27a514324a542ae174a50e909ee2e678cdda43e
0x-2FA <2fa_gh@protonmail.com>
2022-01-14 09:53:47 +0200

    Added error checking

--- main.c
+++ main.c
@@ -7,12 +7,23 @@
 
 int main(void)
 {
+  int num = 0;
+
   struct sockaddr_un address;
 
   int socketfd = CreateClientSocket(AF_UNIX, SOCK_STREAM, 0);
 
   address.sun_family = AF_UNIX;
   strcpy(address.sun_path, "server_socket");
+
+  int con = ConnectToServer(socketfd, (struct sockaddr*) &address, sizeof(address));
+
+  if (con == -1) 
+  {
+    printf("Error: Client 1!\n");
+    return -1;
+  }
   
+
   return 0;
 }
