b0f7c27aeda41e5d79c1efd7d9d385a227957bfb
0x-2FA <2fa_gh@protonmail.com>
2022-01-19 11:44:35 +0200

    Can create new socket for the server

--- server.c
+++ server.c
@@ -4,6 +4,12 @@
 #include <sys/un.h>
 #include <unistd.h>
 
+int CreateServerSocket(int domain, int type, int protocol)
+{
+  int server_sckt = socket(domain, type, protocol);
+  return server_sckt;
+}
+
 void RemoveSockets(const char *socket_name)
 {
   unlink(socket_name);
