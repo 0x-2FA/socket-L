f36169c423406c1b1644cfa1005ed63956231c40
0x-2FA <2fa_gh@protonmail.com>
2022-01-22 17:08:49 +0200

    Added a queue to the server

--- server.h
+++ server.h
@@ -8,7 +8,7 @@
 int CreateServerSocket(int domain, int type, int protocol);
 int NameTheSocket(struct sockaddr_un *server_address, int domain, int server_socketfd);
 int Listen(const int *server_socketfd);
+void Queue(struct sockaddr_un *client_address, int client_socketfd, int server_socketfd);
 void RemoveSockets(const char *socket_name);
-
 
 #endif
