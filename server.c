6fcd44b4570977d3178e2273446d3ee593614592
0x-2FA <2fa_gh@protonmail.com>
2022-01-22 17:11:39 +0200

    Implemented the queue functionality of the server

--- server.c
+++ server.c
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 int CreateServerSocket(int domain, int type, int protocol)
 {
@@ -25,4 +27,22 @@ int Listen(const int *server_socketfd)
 void RemoveSockets(const char *socket_name)
 {
   unlink(socket_name);
+}
+
+void Queue(struct sockaddr_un *client_address, int client_socketfd, int server_socketfd)
+{
+  while (true)
+  {
+    int num;
+    printf("Server is waiting...\n");
+
+    unsigned int client_len = sizeof(*client_address);
+    client_socketfd = accept(server_socketfd, (struct sockaddr *)client_address, &client_len);
+
+    read(client_socketfd, &num, sizeof(num));
+    num++;
+
+    write(client_socketfd, &num, sizeof(num));
+    close(client_socketfd);
+  }
 }
