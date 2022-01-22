c85cef1f06f6117981a2a23861750a9eb8edb24f
0x-2FA <2fa_gh@protonmail.com>
2022-01-22 16:58:26 +0200

    Implemented the Listen function

--- server.c
+++ server.c
@@ -13,11 +13,16 @@ int CreateServerSocket(int domain, int type, int protocol)
 int NameTheSocket(struct sockaddr_un *server_address, int domain, int server_socketfd)
 {
   server_address->sun_family = domain;
-  int length = sizeof(server_address);
+  int length = sizeof(*server_address);
   return bind(server_socketfd, (const struct sockaddr *)server_address, length);
+}
+
+int Listen(const int *server_socketfd)
+{
+  return listen(*server_socketfd, 5);
 }
 
 void RemoveSockets(const char *socket_name)
 {
   unlink(socket_name);
-} 
+}
